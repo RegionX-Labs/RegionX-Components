8adb1928106216006f72b1763bbb059e041f1937
Sergej <sakacszergej@gmail.com>
2024-09-13 15:09:06 +0200

    working

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    //Specifies that the output of the build will be a library.
     lib: {
+      //Defines the entry point for the library build. It resolves 
+      //to src/index.ts,indicating that the library starts from this file.
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "components",
+      name: "react-jp-ui",
+      //A function that generates the output file
+      //name for different formats during the build
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
@@ -14,11 +20,15 @@ export default defineConfig({
       output: {
         globals: {
           react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
+    //Generates sourcemaps for the built files,
+    //aiding in debugging.
     sourcemap: true,
+    //Clears the output directory before building.
     emptyOutDir: true,
   },
-  plugins: [react()],
+  plugins: [react(), dts()],
 });
