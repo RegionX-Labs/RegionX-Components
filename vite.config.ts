eb6c155f4763f843289f85e2d6a4d7d99ba03af0
Sergej <sakacszergej@gmail.com>
2024-09-15 15:00:14 +0200

    progress?

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,40 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
-    //Specifies that the output of the build will be a library.
     lib: {
-      //Defines the entry point for the library build. It resolves 
-      //to src/index.ts,indicating that the library starts from this file.
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "react-jp-ui",
-      //A function that generates the output file
-      //name for different formats during the build
-      fileName: (format) => `index.${format}.js`,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ComponentLibrary',
+      fileName: (format) => `component-library.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
-    //Generates sourcemaps for the built files,
-    //aiding in debugging.
-    sourcemap: true,
-    //Clears the output directory before building.
-    emptyOutDir: true,
   },
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  css: {
+    modules: {
+      // Customize CSS Modules options here if needed
+      localsConvention: 'camelCaseOnly', // or 'camelCase', 'dashes', etc.
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/variables";`, // if you have global SCSS variables
+      },
+    },
+  },
 });
